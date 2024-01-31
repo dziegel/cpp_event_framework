f28059d2a2acb9e1ca32f2403f37d5a004cf57fc
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2024-01-31 08:30:28 +0100

    Cleanup concepts

--- include/cpp_event_framework/Concepts.hxx
+++ include/cpp_event_framework/Concepts.hxx
@@ -10,33 +10,28 @@
 
 #pragma once
 
+#include <cstddef>
 #include <type_traits>
 
 namespace cpp_event_framework
 {
 template <typename T>
-concept BasicLockable = requires(T a) {
+concept Mutex = std::is_constructible_v<T> && std::is_destructible_v<T> && requires(T a) {
     {
         a.lock()
-    } -> std::convertible_to<void>;
+    };
     {
         a.unlock()
-    } -> std::convertible_to<void>;
+    };
 };
 
 template <typename T>
-concept Mutex = std::is_constructible_v<T> && std::is_destructible_v<T> && BasicLockable<T>;
-
-template <typename T>
-concept BasicSemaphore = requires(T a) {
+concept Semaphore = std::is_constructible_v<T, std::ptrdiff_t> && std::is_destructible_v<T> && requires(T a) {
     {
         a.acquire()
-    } -> std::convertible_to<void>;
+    };
     {
         a.release()
-    } -> std::convertible_to<void>;
+    };
 };
-
-template <typename T>
-concept Semaphore = std::is_constructible_v<T, ptrdiff_t> && std::is_destructible_v<T> && BasicSemaphore<T>;
 } // namespace cpp_event_framework
